ebcd5baf5091831a8291b428dec4827054758c19
unknown <sandicoding@gmail.com>
2020-04-01 00:28:48 +0700

    create Admin View

---
+++ src/views/Admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="admin">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Hero from '@/components/Hero.vue'
+// import Products from "@/sections/Products.vue";
+// import Login from "@/components/Login.vue";
+
+export default {
+  name: 'admin',
+  components: {
+     
+  }
+}
+</script>
