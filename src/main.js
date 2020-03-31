764ce31290b87b804a2fc482d4864282ab923c46
unknown <sandicoding@gmail.com>
2020-04-01 00:15:06 +0700

    create componet Hero and Login and create section product

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
+import 'popper.js'
+
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+    // Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
+
+
+//import navbar
+Vue.component('Navbar', require('./components/Navbar.vue').default)
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
